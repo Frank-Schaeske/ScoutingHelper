eabc15c554ff909a1f34d4ccfb7b0ab18dfd40f0
Frank Schaeske <frank.schaeske@web.de>
2023-07-26 11:21:44 +0200

    add link to homepage

--- pages/add.js
+++ pages/add.js
@@ -1,10 +1,18 @@
 import PlayerDetails from "../components/PlayerDetails";
+import Link from "next/link";
+import styled from "styled-components";
 
 export default function Add({ searchedPlayer }) {
-
   return (
     <main>
       <PlayerDetails player={searchedPlayer} />
+      <Link href="/">
+        <StyledLink>New Search</StyledLink>
+      </Link>
     </main>
   );
 }
+
+const StyledLink = styled.a`
+  margin: 20% 20%;
+`;
