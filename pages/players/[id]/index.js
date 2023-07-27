466b28149406f2ee8667503ec236abf4934f3a30
Frank Schaeske <frank.schaeske@web.de>
2023-07-27 15:15:22 +0200

    make player details visible

--- pages/players/[id]/index.js
+++ pages/players/[id]/index.js
@@ -1,14 +1,14 @@
 import { useRouter } from "next/router";
 import PlayerDetailsWithComment from "../../../components/PlayerDetailsWithComment";
+import CommentForm from "../../../components/CommentForm";
 
-export default function PlayerPage({ player }) {
+export default function PlayerPage({ players }) {
   const router = useRouter();
   const { id } = router.query;
 
   return (
     <main>
-      {/*<PlayerDetailsWithComment player={player} />*/}
-      <p>ID: {id}</p>
+      <PlayerDetailsWithComment id={id} players={players} />
     </main>
   );
 }
