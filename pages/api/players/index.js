e678b9be06cc6dfe2f92aa5f4b36d76e524c1665
Frank Schaeske <frank.schaeske@web.de>
2023-08-01 10:27:30 +0200

    write request handlter

--- pages/api/players/index.js
+++ pages/api/players/index.js
@@ -1,5 +1,14 @@
-import { initialPlayers } from "../../../lib/db";
+import dbConnect from "../../../components/db/connect";
+import Player from "../../../components/db/models/Player";
 
-export default function handler(request, response) {
-  return response.status(200).json(initialPlayers);
+export default async function handler(request, response) {
+  await dbConnect();
+
+  if (request.method === "GET") {
+    const players = await Player.find();
+    return response.status(200).json(players);
+  } else {
+    return response.status(405).json({ message: "Method not allowed" });
+  }
+
 }
