e9bc82e46e938acea83fb4e2807d3afa229d061e
Frank Schaeske <frank.schaeske@web.de>
2023-08-01 10:32:50 +0200

    get player list from DB

--- pages/players/index.js
+++ pages/players/index.js
@@ -1,12 +1,16 @@
 import List from "../../components/List";
 import NavigationBar from "../../components/NavigationBar";
 import styled from "styled-components";
+import useSWR from "swr";
 
-export default function Players({ players }) {
+export default function Players() {
+  const { data } = useSWR("/api/players", { fallbackData: [] });
+  console.log(data);
+
   return (
     <>
       <StyledMain>
-        <List players={players} />
+        <List players={data} />
       </StyledMain>
       <NavigationBar />
     </>
