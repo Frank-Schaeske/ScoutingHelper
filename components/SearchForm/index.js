481e801b4c6b61c9144f1fb880628f1a6fd95ccc
Frank Schaeske <frank.schaeske@web.de>
2023-08-04 15:49:17 +0200

    add array with teams and map

--- components/SearchForm/index.js
+++ components/SearchForm/index.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { useRouter } from "next/router";
+import { germanTeams } from "../../lib/db";
 
 export default function SearchForm({ setSearchedPlayer }) {
   const router = useRouter();
@@ -37,26 +38,11 @@ export default function SearchForm({ setSearchedPlayer }) {
       <input type="text" name="search" id="search" minLength="4" />
       <label htmlFor="team">Team</label>
       <select name="team" id="team">
-        <option value="167">1899 Hoffenheim</option>
-        <option value="188">Arminia Bielefeld</option>
-        <option value="168">Bayer Leverkusen</option>
-        <option value="157">Bayern Munich</option>
-        <option value="165">Borussia Dortmund</option>
-        <option value="163">Borussia Monchengladbach</option>
-        <option value="169">Eintracht Frankfurt</option>
-        <option value="170">FC Augsburg</option>
-        <option value="192">FC Koln</option>
-        <option value="174">FC Schalke 04</option>
-        <option value="164">FSV Mainz 05</option>
-        <option value="159">Hertha Berlin</option>
-        <option value="173">RB Leipzig</option>
-        <option value="160">SC Freiburg</option>
-        <option value="178">SpVgg Greuther Furth</option>
-        <option value="182">Union Berlin</option>
-        <option value="172">VfB Stuttgart</option>
-        <option value="176">VfL BOCHUM</option>
-        <option value="161">VfL Wolfsburg</option>
-        <option value="162">Werder Bremen</option>
+        {germanTeams.map((germanTeam) => (
+          <option key={germanTeam.teamID} value={germanTeam.teamID}>
+            {germanTeam.name}
+          </option>
+        ))}
       </select>
       <label htmlFor="season">Season</label>
       <select name="season" id="season">
