278a0ff8b5094a10e292f486884a0c9d0357ca83
Frank Schaeske <frank.schaeske@web.de>
2023-08-07 15:50:07 +0200

    style comment form

--- components/CommentForm/index.js
+++ components/CommentForm/index.js
@@ -7,15 +7,15 @@ export default function CommentForm({ handleSubmit, defaultData }) {
 
   return (
     <StyledForm onSubmit={handleSubmit}>
-      <label htmlFor="comment">Comment:</label>
-      <textarea
+      <StyledLabel htmlFor="comment">Comment:</StyledLabel>
+      <StyledTextarea
         autoFocus
         name="comment"
         id="comment"
         rows="5"
         maxLength="65"
         defaultValue={defaultData?.comment}
-      ></textarea>
+      ></StyledTextarea>
       <StyledButton type="submit">
         <ButtonText>Save</ButtonText>
       </StyledButton>
@@ -26,8 +26,22 @@ export default function CommentForm({ handleSubmit, defaultData }) {
 const StyledForm = styled.form`
   display: flex;
   flex-direction: column;
-  justify-content: space-evenly;
+  align-items: center;
   width: 200px;
-  height: 150px;
+  height: 250px;
   margin: 10px 16%;
+`;
+
+const StyledTextarea = styled.textarea`
+  width: 320px;
+  height: 100px;
+  padding: 0px 35px;
+  border: 1px solid rgba(255, 255, 255, 0.25);
+  border-radius: 20px;
+  background-color: rgba(255, 255, 255, 0.45);
+  box-shadow: 0 0 10px 1px rgba(0, 0, 0, 0.25);
+`;
+
+const StyledLabel = styled.label`
+  margin: 5px;
 `;
