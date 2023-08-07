f162e941f55603a71a20e80efda3740acf4be6f6
Frank Schaeske <frank.schaeske@web.de>
2023-08-07 14:44:27 +0200

    modify styling for header and navigation

--- components/Header/index.js
+++ components/Header/index.js
@@ -14,13 +14,16 @@ export default function Header() {
 const StyledHeader = styled.header`
   background: var(--primary-color);
   color: #ffffff;
-  height: 80px;
+  height: 60px;
   padding: 0px 20px;
   position: fixed;
   top: 0;
   right: 0;
   left: 0;
   z-index: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const StyledLink = styled(Link)`
@@ -30,5 +33,4 @@ const StyledLink = styled(Link)`
 const StyledHeadline = styled.h1`
   color: #ffffff;
   text-decoration: none;
-  text-align: center;
 `;
