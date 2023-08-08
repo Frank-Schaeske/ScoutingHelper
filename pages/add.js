8c3b07eb812ced45b5e81e1e6f84b9f6ed204deb
Frank Schaeske <frank.schaeske@web.de>
2023-08-08 11:43:36 +0200

    improve styling

--- pages/add.js
+++ pages/add.js
@@ -4,6 +4,7 @@ import styled from "styled-components";
 import { useRouter } from "next/router";
 import CommentForm from "../components/CommentForm";
 import useSWR from "swr";
+import { StyledLink } from "../components/StyledLink";
 
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 
@@ -53,16 +54,32 @@ export default function AddPage({ searchedPlayer }) {
           searchedPlayer.statistics[0].league.season
     )
   ) {
+    const searchedPlayerInDB = players.find(
+      (player) =>
+        player.player.name === searchedPlayer.player.name &&
+        player.statistics[0].league.season ===
+          searchedPlayer.statistics[0].league.season
+    );
+
     return (
       <StyledMain>
         <PlayerDetails player={searchedPlayer} />
-        <p>duplicate</p>
         <CommentForm
           handleSubmit={handleSubmit}
           buttonText="Save Player"
           linkText="New Search"
           linkTarget="/"
         />
+        <StyledModal>
+          <p>This player/season combination is already saved.</p>
+          <LinkContainer>
+            <StyledLink href="/">New Search</StyledLink>
+            <StyledLink href={`/players/${searchedPlayerInDB._id}`}>
+              View Player
+            </StyledLink>
+          </LinkContainer>
+        </StyledModal>
+        <StyledOverlay />
       </StyledMain>
     );
   } else if (searchedPlayer) {
@@ -96,4 +113,41 @@ const StyledMain = styled.main`
 
 const StyledParagraph = styled.p`
   margin: 150px 16%;
+`;
+
+const StyledModal = styled.div`
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  width: 300px;
+  border-radius: 20px;
+  transform: translate(-50%, -50%);
+  background: white;
+  padding: 20px;
+  box-shadow: 0px 2px 10px rgba(0, 0, 0, 0.1);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 10px;
+  z-index: 2;
+`;
+
+const LinkContainer = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  gap: 10px;
+
+  button {
+    margin-top: 10px;
+  }
+`;
+
+const StyledOverlay = styled.div`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0, 0, 0, 0.8);
+  z-index: 1;
 `;
