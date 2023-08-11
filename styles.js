8ecaa9cbc31fa78049dbc8c3b34edd49b956edc7
Frank Schaeske <frank.schaeske@web.de>
2023-08-11 14:30:02 +0200

    import roboto font

--- styles.js
+++ styles.js
@@ -1,4 +1,17 @@
 import { createGlobalStyle } from "styled-components";
+import { Roboto_Flex } from "next/font/google";
+
+const roboto = Roboto_Flex({
+  subsets: [
+    "cyrillic",
+    "cyrillic-ext",
+    "greek",
+    "latin",
+    "latin-ext",
+    "vietnamese",
+  ],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+});
 
 export default createGlobalStyle`
 :root {
@@ -12,6 +25,6 @@ export default createGlobalStyle`
   }
   body {
     margin: 0;
-    font-family: system-ui;
+    font-family: ${roboto.style.fontFamily}, Arial, sans-serif;
   }
 `;
